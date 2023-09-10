cec918cc78425cb01a2c6d75af517df56af7f05c
Kimiblock <pn3535@icloud.com>
2023-09-10 21:13:12 +0800

    Try to restart dbus-monitor if receiving EOF

--- snotify.go
+++ snotify.go
@@ -14,34 +14,44 @@ var (
 )
 
 func monitorDbus() {
-	cmd := exec.Command("dbus-monitor", "path='/org/freedesktop/Notifications',interface='org.freedesktop.Notifications',member='Notify'")
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Println("Error creating stdout pipe:", err)
-		return
-	}
-
-	if err := cmd.Start(); err != nil {
-		fmt.Println("Error starting dbus-monitor:", err)
-		return
-	}
-
-	buf := make([]byte, 1024) // Limit the line length to 1024 bytes
-
 	for {
-		n, err := stdout.Read(buf)
+		cmd := exec.Command("dbus-monitor", "path='/org/freedesktop/Notifications',interface='org.freedesktop.Notifications',member='Notify'")
+		stdout, err := cmd.StdoutPipe()
 		if err != nil {
-			fmt.Println("Error reading from dbus-monitor:", err)
-			break
+			fmt.Println("Error creating stdout pipe:", err)
+			return
 		}
 
-		line := string(buf[:n])
-		if strings.Contains(line, "member=Notify") {
-			// Lock the mutex to safely update lastLine
-			mu.Lock()
-			lastLine = line
-			mu.Unlock()
+		if err := cmd.Start(); err != nil {
+			fmt.Println("Error starting dbus-monitor:", err)
+			return
 		}
+
+		buf := make([]byte, 1024) // Limit the line length to 1024 bytes
+
+		for {
+			n, err := stdout.Read(buf)
+			if err != nil {
+				fmt.Println("Error reading from dbus-monitor:", err)
+				break
+			}
+
+			line := string(buf[:n])
+			if strings.Contains(line, "member=Notify") {
+				// Lock the mutex to safely update lastLine
+				mu.Lock()
+				lastLine = line
+				mu.Unlock()
+			}
+		}
+
+		// Wait for the command to finish
+		if err := cmd.Wait(); err != nil {
+			fmt.Println("dbus-monitor exited with an error:", err)
+		}
+
+		// Sleep for a while before restarting dbus-monitor
+		time.Sleep(2 * time.Second)
 	}
 }
 
